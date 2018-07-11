5a0f90bd721b2a670820c16b8d2e71a8fc0ecb37
Pedro Cardouzo <pedro.cardouzo@gmail.com>
2018-07-11 20:28:39 -0300

    little refactoring on CommandLineInterface (syntax_help string variable is now inside constants)

--- constants.py
+++ constants.py
@@ -1,2 +1,56 @@
+version = '0.2'
 base_filepath = "../SAP_XMLs/"
 config_filepath = "../work_area/"
+syntax_help = """
+# extracts from file using template
+extract from <source_file> using <template_file> to <output_file>
+:: <source_file> <template_file> <output_file>  # shortcut for the above
+# example: extract from in.xml using templ.xml to out.xml
+------------------ in.xml ------------------
+<root>
+    <b>
+        <x>5</x>
+        <y>teste</y>
+    </b>
+    <b>
+        <x>not5</x>
+        <y>teste2</y>
+    </b>
+</root>
+------------------ templ.xml ------------------
+<b>
+    <y></y>
+</b>
+------------------ out.xml ------------------
+<b>
+    <y>teste</y>
+</b>
+<b>
+    <y>teste2</y>
+</b>
+
+# filters from <source_file> the occurrences of <candidate> which do not pass the restraining '<field> <comp> <value>'
+filter <source_file> to <output_file> removing <candidate> if <field> <comp> <value> is false
+$ <source_file> <output_file> <candidate> <field> <comp> <value>  # shortcut for the above
+    where
+        <comp> is the comparison operator operator, which can be ==, =, !=, /=, >, >=, <, <=
+# example: filter in.xml to out.xml removing b if x != 5 is false
+------------------ in.xml ------------------
+<root>
+    <b>
+        <x>5</x>
+        <y>teste</y>
+    </b>
+    <b>
+        <x>not5</x>
+        <y>teste2</y>
+    </b>
+</root>
+------------------ out.xml ------------------
+<root>
+    <b>
+        <x>not5</x>
+        <y>teste2</y>
+    </b>
+</root>
+"""
--- src/CommandLineInterface.py
+++ src/CommandLineInterface.py
@@ -8,61 +8,6 @@ import src.XMLExtractor as XMLExtractor
 import src.XMLFilter as XMLFilter
 import src.XMLUtil as XMLUtil
 import src.CommandLineOperators as CommandLineOperators
-import operator
-
-syntax_help = """
-# extracts from file using template
-extract from <source_file> using <template_file> to <output_file>
-:: <source_file> <template_file> <output_file>  # shortcut for the above
-# example: extract from in.xml using templ.xml to out.xml
------------------- in.xml ------------------
-<root>
-    <b>
-        <x>5</x>
-        <y>teste</y>
-    </b>
-    <b>
-        <x>not5</x>
-        <y>teste2</y>
-    </b>
-</root>
------------------- templ.xml ------------------
-<b>
-    <y></y>
-</b>
------------------- out.xml ------------------
-<b>
-    <y>teste</y>
-</b>
-<b>
-    <y>teste2</y>
-</b>
-
-# filters from <source_file> the occurrences of <candidate> which do not pass the restraining '<field> <comp> <value>'
-filter <source_file> to <output_file> removing <candidate> if <field> <comp> <value> is false
-$ <source_file> <output_file> <candidate> <field> <comp> <value>  # shortcut for the above
-    where
-        <comp> is the comparison operator operator, which can be ==, =, !=, /=, >, >=, <, <=
-# example: filter in.xml to out.xml removing b if x != 5 is false
------------------- in.xml ------------------
-<root>
-    <b>
-        <x>5</x>
-        <y>teste</y>
-    </b>
-    <b>
-        <x>not5</x>
-        <y>teste2</y>
-    </b>
-</root>
------------------- out.xml ------------------
-<root>
-    <b>
-        <x>not5</x>
-        <y>teste2</y>
-    </b>
-</root>
-"""
 
 
 def parse(input_string):
@@ -74,10 +19,13 @@ def parse(input_string):
         parse_for_call_filter(input_string)
     elif re.match('^\$ ', input_string):
         call_filter(input_string[2:].split(' '))
-    elif input_string == 'exit':
-        return input_string
     elif input_string == 'help':
-        print(syntax_help)
+        print(constants.syntax_help)
+    elif input_string == 'version':
+        print(constants.version)
+    elif input_string == 'exit':
+        print("closing program")
+        return input_string
     else:
         raise InvalidCommandException(input_string)
 
